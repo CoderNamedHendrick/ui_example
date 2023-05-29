3946cbc07133f328723555aa75e9c758f3f975ec
Sebastine Odeh <sebastinesoacatp@gmail.com>
2023-05-29 09:01:56 +0100

    range picker example

--- lib/views/form_page.dart
+++ lib/views/form_page.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:practice/constants.dart';
 import 'package:practice/widgets/dropdown.dart';
 
-class FormPage extends StatelessWidget {
+class FormPage extends StatefulWidget {
   const FormPage({Key? key}) : super(key: key);
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  String? dropdownValue;
+  int? daysAbsent;
 
   @override
   Widget build(BuildContext context) {
@@ -17,15 +25,37 @@ class FormPage extends StatelessWidget {
         child: Column(
           children: [
             CustomDropdown(
-              dropdownValue: null,
+              dropdownValue: dropdownValue,
               withHint: false,
-              onChanged: (value) {},
+              onChanged: (value) {
+                setState(() {
+                  dropdownValue = value;
+                });
+              },
               items: const [
                 'Missed my loml',
                 'Have to see my loml',
                 'Went to see my loml'
               ],
             ),
+            ListTile(
+              title: const Text('Range picker'),
+              trailing: daysAbsent != null ? Text('$daysAbsent days') : null,
+              onTap: () async {
+                final range = await showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                  initialEntryMode: DatePickerEntryMode.calendarOnly,
+                );
+
+                if (range != null) {
+                  setState(() {
+                    daysAbsent = range.end.difference(range.start).inDays;
+                  });
+                }
+              },
+            )
           ],
         ),
       ),
